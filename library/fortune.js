c0d3f8b602deae062b2cbb935e7cb44b6204e280
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-16 20:59:00 +0000

    Created fortune-teller.js file

--- library/fortune.js
+++ library/fortune.js
@@ -2,6 +2,12 @@ function getFortune(){
     
     var allFortune = ["fortune1","fortune2","fortune3","fortune4"];
     var randomFortune = allFortune[Math.floor(Math.random()*allFortune.length)];
-    console.log(randomFortune);
+    return randomFortune;
     
 }
+
+module.exports = {
+    
+    getFortune: getFortune
+    
+};
