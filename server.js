859897402c5b62f539ea8bfabe0b8545a5192ab2
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-16 22:01:38 +0000

    Created server.js file with basic web server

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var requestListener = function (req, response) {
+  
+    response.writeHead(200);
+    response.end('Hello world!');           
+};
+
+var server = http.createServer(requestListener);
+
+server.listen(process.env.PORT, process.env.IP);
