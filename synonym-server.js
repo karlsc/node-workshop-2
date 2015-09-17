6ace6d43eab98b51910969a7c50443ae5c5a82fb
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-17 15:22:53 +0000

    Updated synonym server to output specific stuff more beautifuly

--- synonym-server.js
+++ synonym-server.js
@@ -5,17 +5,25 @@ var requestListener = function (req, response) {
   
     var userWord = req.url.substring(1);
   
-    request('http://words.bighugelabs.com/api/2/11c036dfbb595f828a10c807a507e679/' + userWord+"/", function(err, res, body) {
-        console.log(err, res, body);
-        var result = body[0];
-        
-        if(result){
+    request('http://words.bighugelabs.com/api/2/11c036dfbb595f828a10c807a507e679/' + userWord+"/json", function(err, res, body) {
+        //console.log(err, res, body);
+        if(err){
             response.writeHead(200);
-            response.end(body);
+            response.end(err);
         } else {
-            response.writeHead(200);
-            response.end("Wrong word selection!");
-        }
+            var result = body[0];
+        
+            if(result){
+                var format = JSON.parse(body);
+                var nounSyn = ((format.noun.syn).map(function(x) { return x[0].toUpperCase() + x.slice(1); })).join(", ");
+                var verbSyn = ((format.verb.syn).map(function(x) { return x[0].toUpperCase() + x.slice(1); })).join(", ");
+                response.writeHead(200);
+                response.end("Noun synonyms:\n"+nounSyn+".\n\nVerb synonyms:\n"+verbSyn+".");
+            } else {
+                response.writeHead(200);
+                response.end("Wrong word selection!");
+            }
+        }    
     });
 };
 
