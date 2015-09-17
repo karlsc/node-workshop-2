ebc899aa7b7244bf29241e23435d3742027e92e4
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-09-17 19:08:54 +0000

    Cached-location.js is working properly on Cloud9 browser

--- cached-location-server.js
+++ cached-location-server.js
@@ -8,7 +8,12 @@ var requestListener = function (req, response) {
     var userCityCap = userCity.charAt(0).toUpperCase() + userCity.slice(1);
     console.log(userCity);
     console.log(cachedLocations);
-    if(!cachedLocations.hasOwnProperty(userCity)){
+    
+    if(userCity === "favicon.ico"){
+        response.writeHead(200);
+        response.end("");
+        
+    } else if(!cachedLocations.hasOwnProperty(userCity)){
         
         request('https://maps.googleapis.com/maps/api/geocode/json?address=' + userCity +'&key=AIzaSyDt_OVnANCrxhc-_a5k9j5mV3dkakhpL30', function(err, res, body) {
         
@@ -35,33 +40,6 @@ function roundValue(x){
     return Math.floor(x*100)/100;
 }  
     
-    
-//    request('https://maps.googleapis.com/maps/api/geocode/json?address=' + userCity +'&key=AIzaSyDt_OVnANCrxhc-_a5k9j5mV3dkakhpL30', function(err, res, body) {
-//        
-//        console.log(err, res, body);
-//        if (err) {
-//            response.writeHead(200);
-//            response.end('Error information:' + err);           
-//        } else {
-//            var data = JSON.parse(body);
-//            var firstResult = data.results[0];
-//
-//            if(firstResult && !cachedLocations.hasOwnProperty(userCity)){
-//                var location = data.results[0].geometry.location;
-//                cachedLocations[userCity] = location;
-//                response.writeHead(200);
-//                response.end(userCityCap+' is located at '+Math.floor(data.results[0].geometry.location.lat*100)/100+' & '+Math.floor(data.results[0].geometry.location.lng*100)/100+".\nFresh response.");
-//            } else if(firstResult){
-//                response.writeHead(200);
-//                response.end(userCityCap+' is located at '+Math.floor(data.results[0].geometry.location.lat*100)/100+' & '+Math.floor(data.results[0].geometry.location.lng*100)/100+".\nCached response.");
-//            } else{
-//                response.writeHead(200);
-//                response.end('Invalid city selected.'); 
-//            }
-//        }
-//    }
-//)};
-
 var server = http.createServer(requestListener);
 
 server.listen(process.env.PORT, process.env.IP);
